2d6233f7eb273abdf38b3a60345d2f743246b23a
warkruid <warkruid@gmail.com>
2023-07-23 19:36:58 +0200

    Rewrite of project settings

--- gendict.py
+++ gendict.py
@@ -1,51 +1,64 @@
-# input filename <language1>-<language2>.csv
-# first line <language1>\t<language2>
+"""
+input filename <language1>-<language2>.csv
+first line <language1>\t<language2>
+"""
 
-import os
+# pylint: disable=C0103
+# pylint: disable=C0301
+
 import getopt
 import sys
-import genanki
 import csv
 import random
+import os
+import genanki
 
-from os.path import basename
 
-options, remainder = getopt.gnu_getopt(sys.argv[1:], 'i:v', ['input=', 'verbose', 'version=', ])
-print('OPTIONS   :', options)
+options, remainder = getopt.gnu_getopt(
+    sys.argv[1:],
+    "i:v",
+    [
+        "input=",
+        "verbose",
+        "version=",
+    ],
+)
+print("OPTIONS   :", options)
 
 for opt, arg in options:
-    if opt in ('-i', '--input'):
+    if opt in ("-i", "--input"):
         input_filename = arg
-    elif opt in ('-v', '--verbose'):
+    elif opt in ("-v", "--verbose"):
         verbose = True
-    elif opt == '--version':
+    elif opt == "--version":
         version = arg
 
-dictionary_name=(os.path.splitext(input_filename)[0])
-[ leftword, rightword ] = dictionary_name.split('-')
+dictionary_name = os.path.splitext(input_filename)[0]
+[leftword, rightword] = dictionary_name.split("-")
 
 SIMPLE_MODEL = genanki.Model(
-        1591007984,
-        'Simple Model',
-        fields=[
-            {'name': 'Question'},
-            {'name': 'Answer'},
-            ],
-        templates=[
-            {
-                'name': 'Card 1',
-                'qfmt': '<center><h1>{{Question}}</h1></center>',
-                'afmt': '<center><h1>{{Question}}</h1></center><center><hr id="answer"><center><h1>{{Answer}}</h1></center>',
-                },
-            ])
+    1591007984,
+    "Simple Model",
+    fields=[
+        {"name": "Question"},
+        {"name": "Answer"},
+    ],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": "<center><h1>{{Question}}</h1></center>",
+            "afmt": '<center><h1>{{Question}}</h1></center><center><hr id="answer"><center><h1>{{Answer}}</h1></center>',
+        },
+    ],
+)
 
-deck_id=random.randrange(1 << 30, 1 << 31)
+deck_id = random.randrange(1 << 30, 1 << 31)
 deck = genanki.Deck(deck_id, dictionary_name)
-with open(input_filename) as csvfile:
-    reader = csv.DictReader(csvfile,delimiter='\t')
+with open(input_filename, encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter="\t")
     for row in reader:
         left_word = row[leftword]
         right_word = row[rightword]
-        deck.add_note(genanki.Note(SIMPLE_MODEL,[left_word, right_word]))
+        deck.add_note(genanki.Note(SIMPLE_MODEL, [left_word, right_word]))
 my_package = genanki.Package(deck)
-my_package.write_to_file('./anki-package/' + dictionary_name + '.apkg')
+my_package.write_to_file("./anki-package/" + dictionary_name + ".apkg")
